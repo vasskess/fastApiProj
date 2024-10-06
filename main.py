1109b5c0274c4c4e3ab26795635818af2f073871
vasskess <vrangelov85@abv.bg>
2024-10-06 14:05:24 +0300

    add pydentic to validate fields

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
 @app.get("/")
 async def root():
     return {"message": "Hello World from my new branch"}
+
+
+@app.post("/createpost")
+def create_post(post: Post):
+    print(post)
+    return {f"{post.title}": f"{post.content}"}
