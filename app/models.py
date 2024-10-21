03a53926e8b199ca9c55f8fa7590386a777d353e
vasskess <vrangelov85@abv.bg>
2024-10-21 21:46:01 +0300

    generated revision with --autogenerate command

--- app/models.py
+++ app/models.py
@@ -17,4 +17,5 @@ class Users(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
+    phone = Column(String, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
